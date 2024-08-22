4728f727f32787a58e6c075e2aa49c5f447768bb
Derek Hudson <dhudsons@amazon.com>
2024-08-22 21:09:34 +0000

    Aws crs v3 tests enabled

--- testing/catalog_test_aws/run-integration-tests.sh
+++ testing/catalog_test_aws/run-integration-tests.sh
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+RUN_V3_TESTS="${RUN_V3_TESTS:-false}"
+FAIL_ON_V3_TESTS="${FAIL_ON_V3_TESTS:-false}"
+
 if [[ "$OSTYPE" == "msys" ]]; then
   python -m pip install --upgrade pip
   python -m pip install --user virtualenv
@@ -19,23 +22,8 @@ if [[ "$OSTYPE" == "msys" ]]; then
   source env/Scripts/activate
   python -m pip install --upgrade pip
   python -m pip install -r requirements.txt
-
-  # Run tests
-  echo ***RUNNING CRS Catalog API V2 TESTS***
-  python run_test_api_v2.py
-  V2_TEST_STATUS=$?
-  echo ***FINISHED CRS Catalog API V2 TESTS***
-
-  echo ***RUNNING CRS Catalog API V3 TESTS***
-  python run_test_api_v3.py
-  V3_TEST_STATUS=$?
-  echo ***FINISHED CRS Catalog API V3 TESTS***
-
-  # python -m pip freeze > requirements.txt
-  python -m pip uninstall -r requirements.txt -y
-
+  PYTHON_CMD=python
 else
-
   echo "Base python path:"
   which python3
   python3 -m venv env
@@ -46,23 +34,29 @@ else
   which python3
   python3 -m pip install --upgrade pip
   python3 -m pip install -r requirements.txt
-
-  # Run tests
-  echo ***RUNNING CRS Catalog API V2 TESTS***
-  python3 run_test_api_v2.py
-  V2_TEST_STATUS=$?
-  echo ***FINISHED CRS Catalog API V2 TESTS***
-
-  # echo ***RUNNING CRS Catalog API V3 TESTS***
-  # python3 run_test_api_v3.py
-  # V3_TEST_STATUS=$?
-  # echo ***FINISHED CRS Catalog API V3 TESTS***
-  echo "Skipping CRS Catalog API V3 test because of this issue with Indexer Service: https://community.opengroup.org/osdu/platform/system/indexer-service/-/issues/185"
-
-  # python3 -m pip freeze > requirements.txt
-  python3 -m pip uninstall -r requirements.txt -y
-
+  PYTHON_CMD=python3
 fi
+
+# Run tests
+echo ***RUNNING CRS Catalog API V2 TESTS***
+$PYTHON_CMD run_test_api_v2.py
+V2_TEST_STATUS=$?
+echo ***FINISHED CRS Catalog API V2 TESTS***
+
+if [[ "$RUN_V3_TESTS" == "true" ]]; then
+  echo ***RUNNING CRS Catalog API V3 TESTS***
+  $PYTHON_CMD run_test_api_v3.py
+  V3_TEST_STATUS=$?
+  echo ***FINISHED CRS Catalog API V3 TESTS***
+  if [[ "$FAIL_ON_V3_TESTS" != "true" ]]; then
+    V3_TEST_STATUS=0
+  fi
+else
+  echo ***SKIPPING CRS Catalog API V3 TESTS***
+fi
+
+# python -m pip freeze > requirements.txt
+$PYTHON_CMD -m pip uninstall -r requirements.txt -y
 
 deactivate
 rm -rf env/
